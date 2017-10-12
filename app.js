9bc1660a7b2d83d7a7c8fd48d7816bc2303c9a7e
shirinR <shirin.raad@gmail.com>
2017-10-11 20:57:36 -0700

    required files

---
+++ app.js
@@ -0,0 +1,10 @@
+$('#submit').on('click', function(){
+
+  var userInput = [{
+    name: $('#name').val(),
+    phone: $('#phone').val(),
+    email: $('#email').val(),
+    unique_id: $('#uid').val()
+  }];
+
+});
---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+var tables = [];
+var waitlist =[];
+
+// Routes
+app.get("/", function(req,res){
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function(req,res){
+  res.sendFile(path.join(__dirname, "make_reservation.html"));
+});
+
+app.get("/viewtable", function(req,res){
+  res.sendFile(path.join(__dirname, "view_tables.html"));
+});
+
+app.post("/api/tables", function(req, res){
+  var newcharacter = req.body;
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  console.log(newcharacter);
+  tables.push(newcharacter);
+  res.json(newcharacter);
+});
+
+app.post("/api/waitlist", function(req, res){
+  var newcharacter = req.body;
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  console.log(newcharacter);
+  waitlist.push(newcharacter);
+  res.json(newcharacter);
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
